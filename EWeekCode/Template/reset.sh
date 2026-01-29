dc9e7404c38838f6ac222b43a8012006bc42df11
John Nguyen <j4nguyen134@gmail.com>
2026-01-29 00:34:53 -0600

    stop controller service when coding

--- EWeekCode/Template/reset.sh
+++ EWeekCode/Template/reset.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 sudo systemctl stop pf_mecanum_gamepad.service
-sudo systemctl disable pf_mecanum_gamepad.service
 cp -f /home/robot/EWeekCode/Template/Control.py  /home/robot/EWeekCode/Template/Model.py /home/robot/EWeekCode/code
